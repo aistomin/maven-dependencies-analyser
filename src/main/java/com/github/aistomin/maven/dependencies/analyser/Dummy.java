0e7ea71a0fe127b76a7f1c5758709f82c2f238bb
aistomin <andrej.istomin.ikeen@gmail.com>
2019-08-04 21:00:14 +0200

    Issue #1. Application skeleton is created.

---
+++ src/main/java/com/github/aistomin/maven/dependencies/analyser/Dummy.java
@@ -0,0 +1,18 @@
+package com.github.aistomin.maven.dependencies.analyser;
+
+/**
+ * Created by aistomin on 2019-08-04.
+ * <p>
+ * Dummy class.
+ */
+public final class Dummy {
+
+    /**
+     * Dummy method.
+     *
+     * @return Dummy value.
+     */
+    public Integer dummy() {
+        return 2 + 2;
+    }
+}
